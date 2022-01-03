a649b8070e5bfb26c9c595b83e26de3cd8e4e7cf
Ivan Zlatov <ivanzlatoff@gmail.com>
2022-01-03 22:25:47 +0000

    Adding a vote() View Function

--- jokes/views.py
+++ jokes/views.py
@@ -1,13 +1,16 @@
+import json
 from django.contrib import messages
 from django.contrib.messages.api import success
+from django.db.models.query_utils import Q
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
     )
-from .models import Joke
+from .models import Joke, JokeVote
 from .forms import JokeForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import JsonResponse
 
 
 class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
@@ -56,6 +59,60 @@ class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+
+
+def vote(request, slug):
+    user = request.user # The logged-in user (or AnonymousUser)
+    joke = Joke.objects.get(slug=slug) # The joke instance
+    data = json.loads(request.body) # Data from the JavaScript
+
+    # Set simple variables
+    vote = data['vote'] # The user's new vote
+    likes = data['likes'] # The number of likes currently displayed on page
+    dislikes = data['dislikes'] # The number of dislikes currently displayed
+
+    if user.is_anonymous: # User not logged in. Can't vote
+        msg = 'Sorry, you have to be logged in to vote.'
+    else: # User is logged in
+        if JokeVote.objects.filter(user=user, joke=joke).exists():
+            # User already voted. Get user's past vote:
+            joke_vote = JokeVote.objects.get(user=user, joke=joke)
+
+            if joke_vote == vote: # User's new vote is the same as old one
+                msg = 'Right. You told us already. Geez.'
+            else: # User changed vote
+                joke_vote.vote = vote # Update JokeVote instance
+                joke_vote.save() # Save
+
+                # Set data to return to the browser
+                if vote == -1:
+                    likes -= 1
+                    dislikes += 1
+                    msg = "Don't like it after all, huh? OK. Noted."
+                else:
+                    likes += 1
+                    dislikes -= 1
+                    msg = 'Grown on you, has it? OK. Noted.'
+        else: # First time user is voting on this joke
+            # Create and save vote
+            joke_vote = JokeVote(user=user, vote=vote)
+            joke_vote.save()
+
+            # Set data to return to the browser
+            if vote == -1:
+                dislikes += 1
+                msg = "Sorry you didn't like the joke."
+            else:
+                likes += 1
+                msg = "Yeah, good one, right?"
+    
+    # Create object to return to the browser
+    response = {
+        'msg': msg,
+        'likes': likes,
+        'dislikes': dislikes
+    }
+    return JsonResponse(response) # Return object as JSON
 
 
 
