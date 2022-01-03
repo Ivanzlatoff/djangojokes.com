06b1f1cdf76f757000e4f667526daccd532b5ad7
Ivan Zlatov <ivanzlatoff@gmail.com>
2022-01-03 21:56:49 +0000

    Adding JS file with Ajax

---
+++ static/js/jokes.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+    if (document.getElementById('like-button')) {
+        const likeButton = document.getElementById('like-button');
+        const dislikeButton = document.getElementById('dislike-button');
+        likeButton.addEventListener('click', () => { register(1); })
+        dislikeButton.addEventListener('click', () => { register(-1); })
+    }
+})
+
+function register(vote) {
+    const csrfInput = document.querySelector("input[name='csrfmiddlewaretoke']");
+    const csrfToken = csrfInput.value;
+    const likes = Number(document.getElementById('likes').innerHTML);
+    const data = {
+        'vote': vote,
+        'likes': likes,
+        'dislikes': dislikes
+    }
+    fetch(ajaxURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const numVotes = data.dislikes + data.likes;
+        let voteText = `${numVotes} vote`;
+        if (numVotes !== 1) voteText += 's';
+        document.getElementById('output').innerHTML = data.msg;
+        document.getElementById('likes').innerHTML = data.likes;
+        document.getElementById('dislikes').innerHTML = data.dislikes;
+        document.getElementById('num-votes').innerHTML = voteText;
+    });
+}
